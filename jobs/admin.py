9cbe1d825d8942990260e39fb90f42ede4e6c07b
terefs <d.van.zurk@gmail.com>
2018-08-01 13:57:42 +0200

    website complete

--- jobs/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Jobs
-admin.site.register(Jobs)
--- jobs/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Jobs(models.Model):
-    image = models.ImageField(upload_to='images/')
-    summary = models.CharField(max_length=200)
-
-class Golf(models.Model):
-    summary = models.CharField(max_length=200)
